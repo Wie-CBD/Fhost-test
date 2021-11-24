8b28668505b32b1706eb13266decdb4a9aabd479
Wie-CBD <wicahya.bagus@gmail.com>
2021-11-25 00:17:54 +0800

    update text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Test PR for Firebase hosting
         </p>
         <a
           className="App-link"
