5807ed1fc7c802acfecdd722229c7d8594ac5eb7
Wie-CBD <wicahya.bagus@gmail.com>
2021-11-25 00:28:46 +0800

    update text in app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test PR for Firebase hosting
+          Test PR for Firebase hosting. Check Cache content policy!
         </p>
         <a
           className="App-link"
